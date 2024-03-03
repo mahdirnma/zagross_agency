cb7b5c4e55f2c31920d6ec9e2f9be647cefb5ca9
mahdi <rahnamamahdi78@gmail.com>
2024-03-04 00:23:30 +0330

    add admin_panel.php

--- admin/admin_panel.php
+++ admin/admin_panel.php
@@ -6,8 +6,29 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Admin Panel</title>
+    <link rel="stylesheet" href="../css/admin.css">
 </head>
 <body>
-div
+<div class="header-border">
+    <div class="tours-border">
+        <table>
+            <tr>
+                <td>حذف</td>
+                <td>ویرایش</td>
+                <td>درجه سختی</td>
+                <td>هزینه</td>
+                <td>تعداد روز</td>
+                <td>روز شروع</td>
+                <td>توضیحات</td>
+                <td>نام تور</td>
+                <td>شماره</td>
+            </tr>
+        </table>
+    </div>
+    <div class="dashboard">
+        <a href="admin_panel.php">مشاهده تورها</a>
+        <a href="admin_panel.php">اضافه کردن تور</a>
+    </div>
+</div>
 </body>
 </html>
